41cf51d2f3802a205a19ab81195cd4b92d17d27a
italovinicios <italovinicios@gmail.com>
2020-06-14 22:59:48 -0300

    alterações de estilo

--- frontEnd/BlobStorageAngular/src/app/app.module.ts
+++ frontEnd/BlobStorageAngular/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
